432f571193010a48b196a194b038d6ed850c767d
Jordan Judt <dev@jordanjudt.com>
2018-01-28 23:35:55 -0600

    nothing yet

--- Android/JCalc/app/src/main/java/com/machone/jcalc/MainActivity.java
+++ Android/JCalc/app/src/main/java/com/machone/jcalc/MainActivity.java
@@ -1,8 +1,7 @@
 package com.machone.jcalc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
@@ -24,33 +23,34 @@ public class MainActivity extends AppCompatActivity {
         registerClickListeners();
     }
 
-    private void registerClickListeners(){
-        View.OnClickListener numberListener = new View.OnClickListener(){
-            public void onClick(View v){
-                expression += ((Button)v).getText().toString();
+    private void registerClickListeners() {
+        View.OnClickListener numberListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                expression += ((Button) v).getText().toString();
                 int length = expression.length();
-                if (length == 2 && expression.startsWith("0")) expression = expression.substring(1, length);
-                ((TextView)findViewById(R.id.input)).setText(expression);
+                if (length == 2 && expression.startsWith("0"))
+                    expression = expression.substring(1, length);
+                ((TextView) findViewById(R.id.input)).setText(expression);
 
                 //findViewById(R.id.inputScroll).scrollTo(findViewById(R.id.input).getRight()+50, 0);
-                HorizontalScrollView inputScroll = (HorizontalScrollView)findViewById(R.id.inputScroll);
+                HorizontalScrollView inputScroll = (HorizontalScrollView) findViewById(R.id.inputScroll);
                 inputScroll.setScrollX(inputScroll.getRight());
 
             }
         };
 
-        View.OnClickListener operatorListener = new View.OnClickListener(){
-            public void onClick(View v){
+        View.OnClickListener operatorListener = new View.OnClickListener() {
+            public void onClick(View v) {
                 int lastIdx = expression.length() - 1;
                 if (lastIdx < 0) return;
 
                 char last = expression.charAt(lastIdx);
-                String operator = ((Button)v).getText().toString();
+                String operator = ((Button) v).getText().toString();
 
                 if (last == '.')
                     expression += "0";
-                else if (last == '+' || last == '-' || last == '×' || last == '÷' || last =='(')
-                    expression = expression.substring(0,lastIdx);
+                else if (last == '+' || last == '-' || last == '×' || last == '÷' || last == '(')
+                    expression = expression.substring(0, lastIdx);
 
                 expression += operator;
                 decimal = false;
@@ -58,14 +58,15 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        View.OnClickListener parenthesisListener = new View.OnClickListener(){
-            public void onClick(View v){
+        View.OnClickListener parenthesisListener = new View.OnClickListener() {
+            public void onClick(View v) {
                 int lastIdx = expression.length() - 1;
                 char last = '\0';
                 if (lastIdx >= 0) last = expression.charAt(lastIdx);
-                String parenthesis = ((Button)v).getText().toString();
+                String parenthesis = ((Button) v).getText().toString();
 
-                if ((lastIdx == -1 && parenthesis.equals(")")) || (parenthesis.equals(")") && last == '(')) return;
+                if ((lastIdx == -1 && parenthesis.equals(")")) || (parenthesis.equals(")") && last == '('))
+                    return;
 
                 expression += parenthesis;
                 decimal = false;
@@ -91,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.leftParenthBtn).setOnClickListener(parenthesisListener);
         findViewById(R.id.rightParenthBtn).setOnClickListener(parenthesisListener);
 
-        findViewById(R.id.decimalBtn).setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        findViewById(R.id.decimalBtn).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 if (decimal) return;
                 int length = expression.length();
                 if (length < 1 || expression.charAt(length - 1) < '0' || expression.charAt(length - 1) > '9')
@@ -100,12 +101,12 @@ public class MainActivity extends AppCompatActivity {
 
                 expression += ".";
                 decimal = true;
-                ((TextView)findViewById(R.id.input)).setText(expression);
+                ((TextView) findViewById(R.id.input)).setText(expression);
             }
         });
 
-        findViewById(R.id.eqBtn).setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        findViewById(R.id.eqBtn).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 if (expression.length() < 1) return;
                 char last = expression.charAt(expression.length() - 1);
                 if (last == '.')
@@ -118,12 +119,10 @@ public class MainActivity extends AppCompatActivity {
                 String result;
                 try {
                     result = String.valueOf(evaluateExpression());
-                }
-                catch(IllegalArgumentException ex){
+                } catch (IllegalArgumentException ex) {
                     result = "ERR";
                     Toast.makeText(MainActivity.this, "Internal error: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-                catch (ArithmeticException ex){
+                } catch (ArithmeticException ex) {
                     result = "UNDEF";
                     Toast.makeText(MainActivity.this, "Cannot divide by 0", Toast.LENGTH_SHORT).show();
                 }
@@ -132,21 +131,21 @@ public class MainActivity extends AppCompatActivity {
                 expression = "";
 
                 if (result.endsWith(".0"))
-                    result = result.substring(0,result.length() - 2);
-                ((TextView)findViewById(R.id.input)).setText(result);
+                    result = result.substring(0, result.length() - 2);
+                ((TextView) findViewById(R.id.input)).setText(result);
             }
         });
 
-        findViewById(R.id.clearBtn).setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        findViewById(R.id.clearBtn).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 decimal = false;
                 expression = "";
-                ((TextView)findViewById(R.id.input)).setText(expression);
+                ((TextView) findViewById(R.id.input)).setText(expression);
             }
         });
 
-        findViewById(R.id.clearEntryBtn).setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        findViewById(R.id.clearEntryBtn).setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 int i = expression.length() - 1;
                 if (i < 1) return;
 
@@ -156,30 +155,30 @@ public class MainActivity extends AppCompatActivity {
                 int div = expression.lastIndexOf('÷');
 
                 if (i == plus || i == minus || i == mult || i == div)
-                    expression = expression.substring(0,i);
+                    expression = expression.substring(0, i);
                 else {
                     if (plus > minus && plus > mult && plus > div)
-                        expression = expression.substring(0,plus+1);
+                        expression = expression.substring(0, plus + 1);
                     else if (minus > plus && minus > mult && minus > div)
-                        expression = expression.substring(0,minus+1);
+                        expression = expression.substring(0, minus + 1);
                     else if (mult > plus && mult > minus && mult > div)
-                        expression = expression.substring(0,mult+1);
+                        expression = expression.substring(0, mult + 1);
                     else if (div > plus && div > minus && div > mult)
-                        expression = expression.substring(0,div+1);
+                        expression = expression.substring(0, div + 1);
                     else
                         expression = "";
                 }
                 decimal = false;
-                ((TextView)findViewById(R.id.input)).setText(expression);
+                ((TextView) findViewById(R.id.input)).setText(expression);
             }
         });
     }
 
-    private float evaluateExpression(){
+    private float evaluateExpression() {
         ArrayList<String> postfix = convertToPostfix();
         Stack<String> calcStack = new Stack<String>();
 
-        for (String token : postfix){
+        for (String token : postfix) {
             char charToken = token.charAt(0);
             if (charToken >= '0' && charToken <= '9')
                 calcStack.push(token);
@@ -197,8 +196,7 @@ public class MainActivity extends AppCompatActivity {
                     if (num2 == 0)
                         throw new ArithmeticException("Cannot divide by 0");
                     result = num1 / num2;
-                }
-                else
+                } else
                     throw new IllegalArgumentException("Unknown token: " + token);
                 calcStack.push(String.valueOf(result));
             }
@@ -206,31 +204,28 @@ public class MainActivity extends AppCompatActivity {
         return Float.parseFloat(calcStack.pop());
     }
 
-    private ArrayList<String> convertToPostfix(){
+    private ArrayList<String> convertToPostfix() {
         Stack<Character> opStack = new Stack<Character>();
         ArrayList<String> postfix = new ArrayList<String>();
 
         char[] split = expression.toCharArray();
         char prevChar = '\0';
-        for (char c : split){
-            if ((c >= '0' && c <= '9') || c == '.'){
+        for (char c : split) {
+            if ((c >= '0' && c <= '9') || c == '.') {
                 String num = "";
                 if ((prevChar >= '0' && prevChar <= '9') || prevChar == '.')
                     num = postfix.remove(postfix.size() - 1);
 
                 postfix.add(num + String.valueOf(c));
-            }
-            else if (c == '('){
+            } else if (c == '(') {
                 opStack.push(c);
-            }
-            else if (c == ')'){
+            } else if (c == ')') {
                 char pop = opStack.pop();
-                while (pop != '('){
+                while (pop != '(') {
                     postfix.add(String.valueOf(pop));
                     pop = opStack.pop();
                 }
-            }
-            else{
+            } else {
                 if (!opStack.isEmpty()) {
                     char prev = opStack.peek();
                     while (((c == '+' || c == '-') && prev != '(')
@@ -244,7 +239,7 @@ public class MainActivity extends AppCompatActivity {
             }
             prevChar = c;
         }
-        while (!opStack.isEmpty()){
+        while (!opStack.isEmpty()) {
             postfix.add(String.valueOf(opStack.pop()));
         }
 
